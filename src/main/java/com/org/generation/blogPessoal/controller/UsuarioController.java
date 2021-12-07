528452718810a6996d710724dc1c061e8c234622
Renomariocosme <renomario.cosme@gmail.com>
2021-12-06 21:04:45 -0300

    Atualização Deploy

--- src/main/java/com/org/generation/blogPessoal/controller/UsuarioController.java
+++ src/main/java/com/org/generation/blogPessoal/controller/UsuarioController.java
@@ -1,6 +1,9 @@
 package com.org.generation.blogPessoal.controller;
 
+import java.util.List;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,8 +13,10 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.org.generation.blogPessoal.model.UserLogin;
@@ -31,6 +36,11 @@ public class UsuarioController {
 	private UsuarioService usuarioService;
 	
 	
+	@GetMapping
+	public ResponseEntity<List<Usuario>> GetAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
 	
 	
 
@@ -49,5 +59,13 @@ public class UsuarioController {
 	public ResponseEntity<Usuario> GetById(@PathVariable long id){
 		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
 	}
+	
+
+	@PutMapping
+	public ResponseEntity<Usuario> Put(@RequestBody @Valid Usuario usuario){
+		return ResponseEntity.ok(repository.save(usuario));
+	}
+	
+
 }
 
