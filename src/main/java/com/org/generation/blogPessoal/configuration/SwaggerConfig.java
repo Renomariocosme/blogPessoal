24488844adaf3d5a7b66237cb209b8fd4bb3b5b9
Renomário <91260015+Renomariocosme@users.noreply.github.com>
2022-05-11 15:04:20 -0300

    Update SwaggerConfig.java

--- src/main/java/com/org/generation/blogPessoal/configuration/SwaggerConfig.java
+++ src/main/java/com/org/generation/blogPessoal/configuration/SwaggerConfig.java
@@ -38,8 +38,8 @@ public class SwaggerConfig {
 	public static ApiInfo metadata() {
 		
 		return new ApiInfoBuilder()
-				.title("api_lojagames")
-				.description("Projeto api spring - lojagames")
+				.title("api_blogPessoal")
+				.description("Projeto api spring - blogPessoal")
 				.version("1.0.0")
 				.license("apache license version 2.0")
 				.licenseUrl("https://github.com/Renomariocosme/blogPessoal")
